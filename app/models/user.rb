171656ba0e4f5ba8554a1a0eebd5bb42b966aa8d
isukacoffee <isukacoffee4120@gmail.com>
2023-02-14 18:48:26 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :nickname_kana, presence: true
+         
+  has_many :birthdays
+  has_many :pictures
+  has_many :color_papers
+  has_many :comments
+  
 end
+
