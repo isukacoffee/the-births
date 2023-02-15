58b1eaca812a179fe63601a06d2a69593cda8519
isukacoffee <isukacoffee4120@gmail.com>
2023-02-15 23:18:36 +0900

    メールアドレスの一意性のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :nickname_kana, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, , format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
          
   has_many :birthdays
   has_many :pictures
