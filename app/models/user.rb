29318cce22d229934a601fefb7eaf9ecb00d5cdc
isukacoffee <isukacoffee4120@gmail.com>
2023-02-16 20:19:46 +0900

    パスワードのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   validates :nickname_kana, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, {presence: true, length: { minimum: 6 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+  validates :password, format:{ with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
+  validates :password, format:{ with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
          
   has_many :birthdays
   has_many :pictures
