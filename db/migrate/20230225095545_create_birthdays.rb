def3b7ca88a7e43562075d98fb1a5dc3e4a05012
isukacoffee <isukacoffee4120@gmail.com>
2023-02-25 20:59:06 +0900

    birthday modelの追加

--- db/migrate/20230225095545_create_birthdays.rb
+++ db/migrate/20230225095545_create_birthdays.rb
@@ -1,7 +1,9 @@
 class CreateBirthdays < ActiveRecord::Migration[6.0]
   def change
     create_table :birthdays do |t|
-
+      t.date        :date,                       null: false
+      t.references  :user,                       null: false, foreign_key: true
+      t.string      :celebrate_person_name,      null: false
       t.timestamps
     end
   end
