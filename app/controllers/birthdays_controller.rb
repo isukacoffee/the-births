656521f6ee3dcaf5f3e03ac9d9477d3b138e925b
isukacoffee <isukacoffee4120@gmail.com>
2023-02-28 21:43:28 +0900

    とりあえず

--- app/controllers/birthdays_controller.rb
+++ app/controllers/birthdays_controller.rb
@@ -3,6 +3,22 @@ class BirthdaysController < ApplicationController
   end
 
   def new
+    @birthday = Birthday.new
+  end
+
+  def create
+    @birthday = Birthday.new(birthday_params)
+    if @birthday.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def birthday_params
+    params.require(:birthday).permit(:date, :user_id, :celebrate_person_name)
   end
   
 end
--- app/models/birthday.rb
+++ app/models/birthday.rb
@@ -1,2 +1,4 @@
 class Birthday < ApplicationRecord
+  validates :date, presence: true
+  validates :celebrate_person_name, presence: true
 end
