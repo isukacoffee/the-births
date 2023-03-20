7ac3c9c8680276a5541a41db64044f644272157e
isukacoffee <isukacoffee4120@gmail.com>
2023-03-21 08:11:21 +0900

    color_paperモデルにバリデーションとアソシエーション

--- app/models/color_paper.rb
+++ app/models/color_paper.rb
@@ -1,2 +1,9 @@
 class ColorPaper < ApplicationRecord
+  validates :question, presence: true
+  validates :question_answer, presence: true
+
+  belongs_to :user  
+  belongs_to :birthday
+  has_many :pictures
+  has_many :comments
 end
