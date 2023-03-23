6f9e35feda4a954768dbb81f692d343118fd22a7
isukacoffee <isukacoffee4120@gmail.com>
2023-03-23 20:24:27 +0900

    color_papersコントローラーの仮記述

--- app/controllers/color_papers_controller.rb
+++ app/controllers/color_papers_controller.rb
@@ -1,2 +1,18 @@
 class ColorPapersController < ApplicationController
+
+  def create
+    @color_paper = Color_paper.new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def color_paper_params
+    params.require(:color_paper).permit(:question, :question_answer, :image).merge(user_id: current_user.id: birthday_id)
+  end
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,5 +5,9 @@ class Picture < ApplicationRecord
   belongs_to :birthday
   belongs_to :color_paper
   has_one_attached :image
+
+  # def was_attached?   画像が存在しなければテキストが必要となり、画像があればテキストは不要(画像のみ)
+  #   self.image.attached?
+  # end
 end
 
