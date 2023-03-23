dc81d649af6cd982d227585e708e424b1a2b28d5
isukacoffee <isukacoffee4120@gmail.com>
2023-03-23 14:11:31 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "birthdays#index"
   
   devise_for :user
-  resources :birthdays, only: [:index, :new, :create, :show, :edit, :update]
+  resources :birthdays, only: [:index, :new, :create, :show, :edit, :update] do
+    resources :color_papers,  only:  [:index, :new, :create]
 
  end
