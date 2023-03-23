e6aec079d285106deb68549e06ecd308fc2b7c9e
isukacoffee <isukacoffee4120@gmail.com>
2023-03-23 19:10:04 +0900

    commentモデルにアソシエーション追加

--- app/models/color_paper.rb
+++ app/models/color_paper.rb
@@ -4,7 +4,7 @@ class ColorPaper < ApplicationRecord
   validates :question, presence: true
   validates :question_answer, presence: true
 
-  belongs_to :user  
+  belongs_to :user
   belongs_to :birthday
   has_many :pictures
   has_many :comments
