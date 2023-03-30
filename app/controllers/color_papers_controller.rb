7c304ac1ee418e404fc4582d57502e679ea79f97
isukacoffee <isukacoffee4120@gmail.com>
2023-03-30 20:22:23 +0900

    あ

--- app/controllers/color_papers_controller.rb
+++ app/controllers/color_papers_controller.rb
@@ -6,11 +6,17 @@ class ColorPapersController < ApplicationController
 
   def create
     @color_paper = Color_paper.new
+    if @color_paper.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   def edit
     @color_paper = Color_paper.find(params[:id])
     unless @birthday.user == current_user
+    end
   end
 
   def update
@@ -22,3 +28,4 @@ class ColorPapersController < ApplicationController
     params.require(:color_paper).permit(:question, :question_answer, :image).merge(user_id :current_user.id :birthday_id)
   end
 end
+
