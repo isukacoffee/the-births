d948f2a50c8603916f08be256c5b5651a9ff2d10
isukacoffee <isukacoffee4120@gmail.com>
2023-04-07 18:05:21 +0900

    コメントコントローラーとコメ欄、写真投稿欄のviewの変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,24 @@
 class CommentsController < ApplicationController
 
   def index
-    @comments = Comments.all
+    @comment = Comment.all
   end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:sentence).merge(user_id: current_user.id)
 end
