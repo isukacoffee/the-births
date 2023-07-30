78a141ed9597c587dfdaa049886337df7b34c76d
isukacoffee <isukacoffee4120@gmail.com>
2023-07-30 18:46:59 +0900

    a

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,23 @@
+class PicturesController < ApplicationController
+
+  def new
+    @picture = Picture.new
+    @color_paper_id = params[:color_paper_id]
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    binding.pry
+    if @picture.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:title, :image, :color_paper_id).merge(user_id: current_user.id)
+  end
+end
